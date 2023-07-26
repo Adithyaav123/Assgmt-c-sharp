9166ff36773bb8cd04c9366061661a1abeac6bce
Adithyaav123 <93602832+Adithyaav123@users.noreply.github.com>
2023-07-26 23:43:46 +0530

    Add files via upload

---
+++ LoopTest.cs
@@ -0,0 +1,15 @@
+using System;
+class MyName
+{
+ public static void Main(string[] args)
+{
+  string name="Adithya";
+  int i=0;
+  while(i<name.Length)
+  {
+    Console.Write(name[i]);
+  i++;
+  }
+ }
+}
+
---
+++ loop.cs
@@ -0,0 +1,13 @@
+using System;
+class MyName
+{
+ public static void Main(string[] args)
+{
+ string name="Adithya";
+ for(int i=0;i<name.Length;i++)
+ {
+  
+   Console.Write(name[i]);
+}
+}
+}
