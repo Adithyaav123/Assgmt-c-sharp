a5c0080e46d35bf30fa9146f7dc3f9ef8fa2de57
Adithyaav123 <93602832+Adithyaav123@users.noreply.github.com>
2023-07-26 23:43:13 +0530

    Add files via upload

---
+++ ArrayTest.cs
@@ -0,0 +1,12 @@
+using System;
+class ArrayTest
+{
+ public static void Main(string[] args)
+{
+ string [] flowers={ "Lotus","Jasmine","Sunflower","Daliya"};
+ for(int i=0;i<flowers.Length;i++)
+{
+ Console.WriteLine(flowers[i]);
+}
+}
+}
---
+++ Arrayb.cs
@@ -0,0 +1,13 @@
+using System;
+class ArrayTestB
+{
+ public static void Main(string[] args)
+{
+ string[] names={"Ammu","Achu","Minnu","Chinnu"};
+
+ foreach(string i in names)
+ {
+   Console.WriteLine(i);
+ }
+}
+}
